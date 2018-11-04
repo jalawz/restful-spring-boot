ad687bd65c3a76742d6f6d7c40dafb1f0413ab25
jalawz <paulomenezes.web@gmail.com>
2018-11-03 22:10:55 -0300

    Lesson 34 - Enhancing Swagger Docs

--- src/main/java/com/in28minutes/rest/webservices/config/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/config/SwaggerConfig.java
@@ -3,17 +3,38 @@ package com.in28minutes.rest.webservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final springfox.documentation.service.Contact DEFAULT_CONTACT =
+            new Contact("Paulo Roberto Menezes", "http://www.paulo.com.br", "paulomenezes.web@gmail.com");
+    public static final ApiInfo DEFAULT_API_INFO =
+            new ApiInfo("Awesom Api title",
+                    "Awesome Api Description",
+                    "1.0", "",
+                    DEFAULT_CONTACT,
+                    "Apache 2.0",
+                    "");
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
     // Bean - Docket
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/domain/User.java
+++ src/main/java/com/in28minutes/rest/webservices/domain/User.java
@@ -1,17 +1,23 @@
 package com.in28minutes.rest.webservices.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "At least two characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
     protected User() {
